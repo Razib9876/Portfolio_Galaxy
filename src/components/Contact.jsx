c567822a7bf86570f9614d9d8874f320f3222842
Razib987 <mohammadrazib987@gmail.com>
2026-02-07 21:51:26 +0600

    last one

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,6 @@ const Contact = () => {
           body: JSON.stringify(form),
         },
       );
-      console.log("API URL:", import.meta.env.VITE_API_URL);
 
       if (!response.ok) {
         throw new Error("Failed to send message");
